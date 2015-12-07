83fb99ccc1857115ccc8ba75ad0b5afde6da87e1
Jay Shenoy <jayshenoy@hotmail.com>
2015-12-06 21:01:49 -0500

    Use Google App Engine

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hour of Crossy Code
+Welcome to the codebase for the Hour of Crossy Code tutorial!
+
+## License
+Content of Hour of Crossy Code (HTML, CSS, images, etc.) is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+
+Codebase of Hour of Crossy Code (JavaScript, Python, etc.) is licensed under the GNU General Public License Version 3.
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, send_file, url_for
+from flask import Flask, render_template, send_file, url_for, request, redirect
 import jinja2
+from google.appengine.ext import ndb
 app = Flask(__name__)
 
 default_code = {}
@@ -14,18 +15,32 @@ default_code['7'] = jinja2.Markup('<xml xmlns="http://www.w3.org/1999/xhtml"><bl
 tasks = {};
 tasks['1'] = 'Add another hop forward command to help Crossy collect the coin.'
 tasks['2'] = 'Program Crossy to collect both coins.'
-tasks['3'] = 'Program Crossy to collect both coins.'
+tasks['3'] = 'Program Crossy to collect both coins. Hint: hop around the blue car.'
 tasks['4'] = 'How quickly can you code?'
 tasks['5'] = 'Use the repeat block and the hop forward command to help Crossy collect the coin.'
-tasks['6'] = 'Help Crossy zigzag his way towards the coin.'
+tasks['6'] = 'Help Crossy zigzag his way towards the coin. Don\'t jump in the water!'
 tasks['7'] = 'Use the repeat block to re-solve the last level.'
+
+class User(ndb.Model):
+    name = ndb.StringProperty()
+    email = ndb.StringProperty()
+
+@app.route('/')
+def home():
+    return render_template('home.html', home=True);
 
 @app.route('/crossy/<level>')
 def crossy(level):
     if level == '8':
         return render_template('congratulations.html')
     else:
-        return render_template('index.html', level=level, defaultCode=default_code[level], task=tasks[level])
+        return render_template('crossy.html', level=level, defaultCode=default_code[level], task=tasks[level])
+
+@app.route('/newUser', methods=['POST'])
+def new_user():
+    new_user = User(name='%s %s' % (request.form['fName'], request.form['lName']), email=request.form['email'])
+    new_user.put()
+    return 'Success!'
 
 @app.route('/crossyAsset/<assetname>')
 def crossyAsset(assetname):
@@ -37,6 +52,3 @@ def crossyAsset(assetname):
 @app.route('/map/<level>')
 def getMap(level):
     return send_file('static/maps/%s.json' % level)
-
-if __name__ == '__main__':
-    app.run(debug=True)
